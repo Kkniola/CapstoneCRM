4dee7879ac7adc59451a628ff7a3b35c483eccdc
Kkniola <79425864+Kkniola@users.noreply.github.com>
2021-06-17 21:35:10 -0400

    Update Company.cs

--- Models/Company.cs
+++ Models/Company.cs
@@ -14,10 +14,6 @@ namespace CapstoneSalesCRM.Models
         [Display(Name="Company Name")]
         public string CompanyName { get; set; }
 
-       
-
-        [Display(Name = "Location")]
-        public int LocationID { get; set; }
 
 
         [Display(Name = "Industry ID")]
